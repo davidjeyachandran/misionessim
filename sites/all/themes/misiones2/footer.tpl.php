215c48af82d8cee4539c9df0190707959382ea1f
David Jeyachandran <davidjeyachandran@gmail.com>
2017-10-18 13:07:06 -0500

    Move footer code to footer template

---
+++ sites/all/themes/misiones2/footer.tpl.php
@@ -0,0 +1,46 @@
+<footer id="footer">
+
+  <div class="middle-footer-wrapper">
+    <div class="container">
+      <div class="middle-footer">
+        <div class="region region-footer">
+          <div id="block-socialicons" class="block block-block-content block-block-contentdba664a4-eedc-4024-b73b-7d5903d71b97">
+            <div class="clearfix text-formatted field field--name-body field--type-text-with-summary field--label-hidden field__item">
+              <section id="social_block">
+                <ul>
+                  <li class="facebook">
+                    <a title="Facebook" href="https://www.facebook.com/SIMLatinoamerica/" target="_blank">
+                    <span>Facebook</span>
+                    </a>
+                  </li>
+                  <li class="rss">
+                    <a title="RSS" href="rss.xml" target="_blank">
+                    <span>RSS</span>
+                    </a>
+                  </li>
+                  <li class="youtube">
+                    <a title="YouTube" href="https://www.youtube.com/user/VamosSIM" target="_blank">
+                    <span>YouTube</span>
+                    </a>
+                  </li>
+                  <li class="email">
+                    <a title="Email" href="/contact" target="_blank">
+                    <span>Email</span>
+                    </a>
+                  </li>
+                </ul>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bottom-footer-wrapper">
+    <div class="container">
+      <div class="bottom-footer">
+        Powered by <a href="#">Misionessim</a>
+      </div>
+    </div>
+  </div>
+</footer>
--- sites/all/themes/misiones2/page--front.tpl.php
+++ sites/all/themes/misiones2/page--front.tpl.php
@@ -89,7 +89,7 @@
 <?php $path =  base_path() . path_to_theme();?>
 
 <div id="toTop"></div>
-<?php include 'header.tpl.php'; ?> 
+<?php include 'header.tpl.php'; ?>
 
 <div id="header-bottom" class="header-bottom-wrapper">
 
@@ -204,50 +204,4 @@
 </div>
 </div>
 
-
-<footer id="footer">
-
-  <div class="middle-footer-wrapper">
-    <div class="container">
-      <div class="middle-footer">
-        <div class="region region-footer">
-          <div id="block-socialicons" class="block block-block-content block-block-contentdba664a4-eedc-4024-b73b-7d5903d71b97">
-            <div class="clearfix text-formatted field field--name-body field--type-text-with-summary field--label-hidden field__item">
-              <section id="social_block">
-                <ul>
-                  <li class="facebook">
-                    <a title="Facebook" href="https://www.facebook.com/SIMLatinoamerica/" target="_blank">
-                    <span>Facebook</span>
-                    </a>
-                  </li>
-                  <li class="rss">
-                    <a title="RSS" href="rss.xml" target="_blank">
-                    <span>RSS</span>
-                    </a>
-                  </li>
-                  <li class="youtube">
-                    <a title="YouTube" href="https://www.youtube.com/user/VamosSIM" target="_blank">
-                    <span>YouTube</span>
-                    </a>
-                  </li>
-                  <li class="email">
-                    <a title="Email" href="/contact" target="_blank">
-                    <span>Email</span>
-                    </a>
-                  </li>
-                </ul>
-              </section>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="bottom-footer-wrapper">
-    <div class="container">
-      <div class="bottom-footer">
-        Powered by <a href="#">Misionessim</a>
-      </div>
-    </div>
-  </div>
-</footer>
+<?php include 'footer.tpl.php'; ?>
