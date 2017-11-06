acc2740b209693ef24f88b1271269b3ee38d1064
David Jeyachandran <davidjeyachandran@gmail.com>
2017-11-06 15:37:46 -0500

    Add JS for Accordian

--- sites/all/themes/misiones2/js/view-1.init.js
+++ sites/all/themes/misiones2/js/view-1.init.js
@@ -1,29 +1,30 @@
 jQuery(document).ready(function(){
+  console.log('Ready running');
 	jQuery("#block-views-block-view-1-block-1 .views-view-grid > .row > .col-sm-4 .views-field-title a").wrapInner("<div><h2></h2></div>");
 	jQuery("#block-views-block-view-1-block-1 .views-view-grid > .row > .col-sm-4 .views-field-title a > div").addClass("item-html");
 
     //Add Inactive Class To All Accordion Headers
-  $('.accordion-header').toggleClass('inactive-header');
+  jQuery('.accordion-header').toggleClass('inactive-header');
   
   //Set The Accordion Content Width
-  var contentwidth = $('.accordion-header').width();
-  $('.accordion-content').css({'width' : contentwidth });
+  var contentwidth = jQuery('.accordion-header').width();
+  jQuery('.accordion-content').css({'width' : contentwidth });
   
   //Open The First Accordion Section When Page Loads
-  $('.accordion-header').first().toggleClass('active-header').toggleClass('inactive-header');
-  $('.accordion-content').first().slideDown().toggleClass('open-content');
+  jQuery('.accordion-header').first().toggleClass('active-header').toggleClass('inactive-header');
+  jQuery('.accordion-content').first().slideDown().toggleClass('open-content');
   
   // The Accordion Effect
-  $('.accordion-header').click(function () {
-    if($(this).is('.inactive-header')) {
-      $('.active-header').toggleClass('active-header').toggleClass('inactive-header').next().slideToggle().toggleClass('open-content');
-      $(this).toggleClass('active-header').toggleClass('inactive-header');
-      $(this).next().slideToggle().toggleClass('open-content');
+  jQuery('.accordion-header').click(function () {
+    if(jQuery(this).is('.inactive-header')) {
+      jQuery('.active-header').toggleClass('active-header').toggleClass('inactive-header').next().slideToggle().toggleClass('open-content');
+      jQuery(this).toggleClass('active-header').toggleClass('inactive-header');
+      jQuery(this).next().slideToggle().toggleClass('open-content');
     }
     
     else {
-      $(this).toggleClass('active-header').toggleClass('inactive-header');
-      $(this).next().slideToggle().toggleClass('open-content');
+      jQuery(this).toggleClass('active-header').toggleClass('inactive-header');
+      jQuery(this).next().slideToggle().toggleClass('open-content');
     }
   });
 
