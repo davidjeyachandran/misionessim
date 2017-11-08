b9a72cb1e213c3879e59d65d941eb1f22d1540f4
David Jeyachandran <davidjeyachandran@gmail.com>
2017-11-08 06:08:54 -0500

    Move logo into menu area

--- sites/all/themes/misiones2/header.tpl.php
+++ sites/all/themes/misiones2/header.tpl.php
@@ -1,42 +1,12 @@
 <?php $path =  base_path() . path_to_theme();?>
-<nav id="top-nav-container" role="navigation">
+
+<nav class="navbar navbar-static-top stickUpTop" id="main-nav-container" role="navigation">
   <div class="container">
-    <div class="navbar-header">
-      <div class="region region-header">
-        <div id="block-bon-voyage-branding" class="block block-system block-system-branding-block">
           <div id="header_logo">
-            <?php if ($logo): ?>
               <a class="logo navbar-btn" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
                 <img src="<?php print $path ?>/images/logo.png" alt="<?php print t('Home'); ?>" />
               </a>
-            <?php endif; ?>
-
           </div>
-        </div>
-      </div>
-    </div>
-    <div id="top-navbar">
-      <div id="top-navbar-row-1">
-        <div class="region region-user-menu">
-          <div aria-labelledby="block-useraccountmenu-menu" id="block-useraccountmenu">
-            <h2 class="visually-hidden">User account menu</h2>
-            <ul class="menu">
-              <li class="menu-item">
-                <div class="region region-search">
-                  <div class="search-block-form block block-search container-inline" data-drupal-selector="search-block-form" id="block-searchform" role="search">
-                    
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</nav>
-<nav class="navbar navbar-static-top stickUpTop" id="main-nav-container" role="navigation">
-  <div class="container">
     <button class="navbar-toggle collapsed" aria-expanded="false" type="button" data-toggle="collapse" data-target="#main-navbar">
     <span class="sr-only">Toggle navigation</span>
     <span class="icon-bar"></span>
