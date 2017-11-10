67c99f1051e3ac1d113a0f902da1c0bc55d9cb11
David Jeyachandran <davidjeyachandran@gmail.com>
2017-11-10 16:14:56 -0500

    Remove dpm and remove loading of unnecessary js

--- sites/all/themes/misiones2/template.php
+++ sites/all/themes/misiones2/template.php
@@ -2,12 +2,9 @@
 
 function misiones2_preprocess_views_view(&$vars) {
    $view = $vars['view'];
-   dpm($view);
 
    // Make sure it's the correct view
   if($view->name == 'revista_vamos_banner') {
-     drupal_add_js(drupal_get_path('theme', 'misiones2') . '/js/vamos-actions.js');
+     // drupal_add_js(drupal_get_path('theme', 'misiones2') . '/js/vamos-actions.js');
   }
 }
-
-?>
