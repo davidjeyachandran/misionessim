335052b6c3d46b351e70cea649fe249786feb30e
David Jeyachandran <davidjeyachandran@gmail.com>
2017-11-13 20:37:32 -0500

    Add Google Analytics JS code to footer

--- sites/all/themes/misiones2/footer.tpl.php
+++ sites/all/themes/misiones2/footer.tpl.php
@@ -33,8 +33,19 @@
   <div class="bottom-footer-wrapper">
     <div class="container">
       <div class="bottom-footer">
-        Powered by <a href="#">Misionessim</a>
+
       </div>
     </div>
   </div>
 </footer>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-12299775-6', 'misionessim.org');
+  ga('send', 'pageview');
+
+</script>
