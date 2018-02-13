54942ba3cf7b216986baa8cf5aa1a38b60f91069
David Jeyachandran <davidjeyachandran@gmail.com>
2018-02-13 11:13:48 -0500

    Changed slider to increase time between slides

--- sites/all/themes/misiones2/js/homeslider.js
+++ sites/all/themes/misiones2/js/homeslider.js
@@ -3,11 +3,11 @@ jQuery(document).ready(function(){
 	if (typeof(homeslider_speed) == 'undefined')
 		homeslider_speed = 500;
 	if (typeof(homeslider_pause) == 'undefined')
-		homeslider_pause = 3000;
+		homeslider_pause = 4000;
 	if (typeof(homeslider_loop) == 'undefined')
 		homeslider_loop = true;
     if (typeof(homeslider_width) == 'undefined')
-        homeslider_width = 2050;
+        homeslider_width = 1920;
 	
 	var tl = new TimelineMax();
 	
