b944be02aebadf49b6fb3aa75d049a3d76c9c48f
David Jeyachandran <davidjeyachandran@gmail.com>
2018-10-10 12:52:47 -0500

    Update faster config

--- sites/all/themes/misiones2/footer.tpl.php
+++ sites/all/themes/misiones2/footer.tpl.php
@@ -54,6 +54,50 @@
 <link type="text/css" rel="stylesheet" href="https://fasterweb.io/faster.css" />
 
 <script>
+function slider(jQuery) {
+
+  if (typeof(homeslider_speed) == 'undefined')
+    homeslider_speed = 500;
+  if (typeof(homeslider_pause) == 'undefined')
+    homeslider_pause = 4000;
+  if (typeof(homeslider_loop) == 'undefined')
+    homeslider_loop = true;
+    if (typeof(homeslider_width) == 'undefined')
+        homeslider_width = 1920;
+  
+  var tl = new TimelineMax();
+  
+  tl.from(".homeslider-description h2", 0.3,{left:-100, autoAlpha:0})
+  .from(".homeslider-description h3", 0.3,{right:-100, autoAlpha:0});
+
+  if (!!jQuery.prototype.bxSlider)
+    jQuery('#homeslider').bxSlider({
+      mode:'fade',
+      useCSS: false,
+      maxSlides: 1,
+      slideWidth: homeslider_width,
+      infiniteLoop: homeslider_loop,
+      hideControlOnEnd: true,
+      pager: true,
+      autoHover: true,
+      autoControls: true,
+      auto: homeslider_loop,
+      speed: parseInt(homeslider_speed),
+      pause: homeslider_pause,
+      controls: false,
+      startText:'',
+      stopText:'',
+      pagerCustom: '',
+      onSliderLoad:function(){tl.play()},
+      onSlideBefore:function(){tl.restart()},
+      onSlideAfter:function(){}
+    });
+
+  jQuery('.homeslider-description').click(function () {
+      window.location.href = jQuery(this).prev('a').prop('href');
+  });
+}
+
   const websiteConfig = {
     "urlInclude": [],
     "urlExclude" : ["/admin/*", "*.doc", "*.docx", "*.pdf", "*.ppt", "*.pptx"],
@@ -61,27 +105,29 @@
     "url": {
       "/": {
         "prefetch" : ["/quienessomos","/recursos", "/la-revista"],
-        "loadFunction": function(){}
+        "pageFunction": function(jQuery){
+          slider(jQuery)
+        }
       },
       "/quienessomos": {
         "prefetch" : ["/", "/recursos-movilicemos"],
-        "loadFunction": function(){}
+        "pageFunction": function(){}
       },
       "/recursos-movilicemos": {
         "prefetch" : ["/", "/quienessomos", "/la-revista"],
-        "loadFunction": function(){}
+        "pageFunction": function(){}
       },
       "/la-revista": {
         "prefetch" : ["/", "/cursovamos"],
-        "loadFunction": function(){}
+        "pageFunction": function(){}
       },
       "/cursovamos": {
         "prefetch" : ["/", "/recursos"],
-        "loadFunction": function(){}
+        "pageFunction": function(urlTarget){console.log('urlTarget: ' + urlTarget)}
       },
       "all": {
         "prefetch" : ["/"],
-        "loadFunction": function(urlTarget, jQuery, ga){
+        "pageFunction": function(urlTarget, jQuery, ga){
           ga('set', 'dimension1','faster');
           ga('send', 'pageview', urlTarget, {location: window.location.href});
           console.log('All url callback: ' + window.location.pathname);
