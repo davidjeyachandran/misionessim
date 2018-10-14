3d67e78a170fe79c6ed94c04c1c65421b85928df
David Jeyachandran <davidjeyachandran@gmail.com>
2018-10-14 14:59:36 -0500

    Make fasterweb js load and run async

--- sites/all/themes/misiones2/footer.tpl.php
+++ sites/all/themes/misiones2/footer.tpl.php
@@ -50,57 +50,58 @@
 
 </script>
 
-<script src="https://fasterweb.io/faster.js"></script>
+<script async src="https://fasterweb.io/faster.js"></script>
 <link type="text/css" rel="stylesheet" href="https://fasterweb.io/faster.css" />
 
 <script>
-function homeSlider(jQuery) {
+window.addEventListener('load', function () {
 
-  if (typeof(homeslider_speed) == 'undefined')
-    homeslider_speed = 500;
-  if (typeof(homeslider_pause) == 'undefined')
-    homeslider_pause = 4000;
-  if (typeof(homeslider_loop) == 'undefined')
-    homeslider_loop = true;
-    if (typeof(homeslider_width) == 'undefined')
-        homeslider_width = 1920;
-  
-  var tl = new TimelineMax();
-  
-  tl.from(".homeslider-description h2", 0.3,{left:-100, autoAlpha:0})
-  .from(".homeslider-description h3", 0.3,{right:-100, autoAlpha:0});
+  function homeSlider(jQuery) {
+    if (typeof(homeslider_speed) == 'undefined')
+      homeslider_speed = 500;
+    if (typeof(homeslider_pause) == 'undefined')
+      homeslider_pause = 4000;
+    if (typeof(homeslider_loop) == 'undefined')
+      homeslider_loop = true;
+      if (typeof(homeslider_width) == 'undefined')
+          homeslider_width = 1920;
+    
+    var tl = new TimelineMax();
+    
+    tl.from(".homeslider-description h2", 0.3,{left:-100, autoAlpha:0})
+    .from(".homeslider-description h3", 0.3,{right:-100, autoAlpha:0});
 
-  if (!!jQuery.prototype.bxSlider)
-    jQuery('#homeslider').bxSlider({
-      mode:'fade',
-      useCSS: false,
-      maxSlides: 1,
-      slideWidth: homeslider_width,
-      infiniteLoop: homeslider_loop,
-      hideControlOnEnd: true,
-      pager: true,
-      autoHover: true,
-      autoControls: true,
-      auto: homeslider_loop,
-      speed: parseInt(homeslider_speed),
-      pause: homeslider_pause,
-      controls: false,
-      startText:'',
-      stopText:'',
-      pagerCustom: '',
-      onSliderLoad:function(){tl.play()},
-      onSlideBefore:function(){tl.restart()},
-      onSlideAfter:function(){}
+    if (!!jQuery.prototype.bxSlider)
+      jQuery('#homeslider').bxSlider({
+        mode:'fade',
+        useCSS: false,
+        maxSlides: 1,
+        slideWidth: homeslider_width,
+        infiniteLoop: homeslider_loop,
+        hideControlOnEnd: true,
+        pager: true,
+        autoHover: true,
+        autoControls: true,
+        auto: homeslider_loop,
+        speed: parseInt(homeslider_speed),
+        pause: homeslider_pause,
+        controls: false,
+        startText:'',
+        stopText:'',
+        pagerCustom: '',
+        onSliderLoad:function(){tl.play()},
+        onSlideBefore:function(){tl.restart()},
+        onSlideAfter:function(){}
+      });
+
+    jQuery('.homeslider-description').click(function () {
+        window.location.href = jQuery(this).prev('a').prop('href');
     });
-
-  jQuery('.homeslider-description').click(function () {
-      window.location.href = jQuery(this).prev('a').prop('href');
-  });
-}
+  }
 
   const websiteConfig = {
     "urlInclude": [],
-    "urlExclude" : ["/admin/*", "*.doc", "*.docx", "*.pdf", "*.ppt", "*.pptx"],
+    "urlExclude" : ["/admin/*", "*.txt", "*.doc", "*.docx", "*.pdf", "*.ppt", "*.pptx", "*.mp3", "*.mp4", "*.avi"],
     "elementSelector": null,
     "url": {
       "/": {
@@ -136,7 +137,8 @@ function homeSlider(jQuery) {
     }
   }
 
-  window.onload = function(){
-    window.faster(websiteConfig, jQuery, ga);
-  }
+  window.faster(websiteConfig, jQuery, ga);
+
+});
+
 </script>
