09084a1d8bfc3a0b4bc4c1e54664f294cbbc88b7
David Jeyachandran <davidjeyachandran@gmail.com>
2018-11-17 17:00:28 -0500

    Remove Faster testing

--- sites/all/themes/misiones2/footer.tpl.php
+++ sites/all/themes/misiones2/footer.tpl.php
@@ -39,9 +39,6 @@
   </div>
 </footer>
 
-
-<?php if(!$logged_in): ?>
-
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -49,78 +46,5 @@
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-12299775-6', 'auto');
-
-</script>
-
-<script async src="https://fasterweb.io/faster.js"></script>
-<script async type="text/javascript" src="https://fasterweb.io/js/ab.js"></script>
-<link type="text/css" rel="stylesheet" href="https://fasterweb.io/faster.css" />
-
-<script>
-window.addEventListener('load', function () {
-
-  const websiteConfig = {
-    "urlInclude": [],
-    "urlExclude" : ["/admin/*", "*.txt", "*.doc", "*.docx", "*.pdf", "*.ppt", "*.pptx", "*.mp3", "*.mp4", "*.avi", "*.jpg", "*.jpeg"],
-    "elementSelector": null,
-    "url": {
-      "/": {
-        "prefetch" : ["/quienessomos","/recursos", "/la-revista"],
-        "pageFunction": function(urlTarget, jQuery, ga){
-          homeSlider(jQuery);
-          lazyLoad();
-        }
-      },
-      "/quienessomos": {
-        "prefetch" : ["/", "/recursos-movilicemos"],
-        "pageFunction": function(){}
-      },
-      "/recursos-movilicemos": {
-        "prefetch" : ["/", "/quienessomos", "/la-revista"],
-        "pageFunction": function(){}
-      },
-      "/la-revista": {
-        "prefetch" : ["/", "/cursovamos"],
-        "pageFunction": function(){}
-      },
-      "/cursovamos": {
-        "prefetch" : ["/", "/recursos"],
-        "pageFunction": function(urlTarget){console.log('urlTarget: ' + urlTarget)}
-      },
-      "all": {
-        "prefetch" : ["/"],
-        "pageFunction": function(urlTarget, jQuery, ga){
-          ga('set', 'dimension1','faster');
-          ga('send', 'pageview', urlTarget, {location: window.location.href});
-          console.log('All url callback: ' + window.location.pathname);
-        }
-      }
-    }
-  }
-
-  //AB Test - Faster and Control
-  var myAbTest = ABTest({
-    name: "Test_Faster",
-    customVarSlot: 1,
-    variations: {
-        faster: function () {
-          console.log('AB: Faster');
-          ga('set', 'dimension1','faster');
-          ga('send', 'pageview');
-          window.faster(websiteConfig, jQuery, ga);
-        },
-        control: function () {
-          console.log('AB: Control');
-          ga('set', 'dimension1','control');
-          ga('send', 'pageview')
-        }
-    }
-  });
-
-});
-
-<?php else: ?>
-No Google Analytics Tracking when logged in.
-<?php endif; ?>
-
+  ga('send', 'pageview');
 </script>
