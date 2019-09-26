e99068b21892bfcb339527ae18f2b2341e389648
David Jeyachandran <davidjeyachandran@gmail.com>
2019-09-25 21:59:41 -0500

    Add msg Haz click para descargar above button

--- sites/all/themes/misiones2/node--magazine_vamos.tpl.php
+++ sites/all/themes/misiones2/node--magazine_vamos.tpl.php
@@ -81,8 +81,8 @@
  */
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
-<div style="padding:30px 0;">
+<p style="padding-top:10px">Haz click para descargar</p>
+<div style="padding-bottom:30px">
 <a class="btn-custom" target="_blank" href="<?php print $content['field_magazinepdf'][0]['#markup']; ?>">
   <div class="texto-descarga">Descarga<span class="display-filename"><?php print $title; ?></span></div>
   <i class="fa fa-download" aria-hidden="true"></i>
